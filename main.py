4ea9899364fb48335b5a732537a3e0b7dee85510
Harsh Tenguriya <thegreatharsh1@gmail.com>
2022-06-22 19:06:06 +0530

    Read and update CSV file from URL

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-# Getting list or scraping html automatically from Webpage
+# Read CSV from URl using pandas and download all the files on website
 
 import pandas as pd
-simpsons = pd.read_html('https://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes_(seasons_1%E2%80%9320)')
-print(len(simpsons))
-print(simpsons[1])
+# Read 1 csv frm website
+df_premier21 = pd.read_csv('https://www.football-data.co.uk/mmz4281/2122/E0.csv')
 
+# showing dataframes
+df_premier21
+
+#rename columns
+df_premier21.rename(columns={'FTHG': 'home_goals','FTAG': 'away_goals'}, inplace=True)
+print(df_premier21)
